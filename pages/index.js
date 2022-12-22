29c176977febf76563977876eeb8aabd3b86b66f
Atsighi Bright <brytebee@gmail.com>
2022-12-22 23:10:43 +0100

    Add bare images

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
+import { Card, CardActionArea, CardMedia, Grid } from "@mui/material";
 import { Layout } from "../components/Layout";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-          <li>Product 4</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );

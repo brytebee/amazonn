fff2dd2fc0130e40ec2bd05bacda10004e2671e2
Atsighi Bright <brytebee@gmail.com>
2022-12-22 22:49:05 +0100

    Add date function and dommy data

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,22 +2,30 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import Head from "next/head";
 import React from "react";
+import date from "../utils/date";
 
+const datae = date;
 export const Layout = ({ children }) => {
-  let date = new Date();
-  date = date.getFullYear();
   return (
     <div>
       <Head>
         <title>amazonn - ecommerce</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" style={{ backgroundColor: "#203040" }}>
         <Toolbar>
           <Typography>amazonn</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>All Right Reserved. @{date} Amazonn.</footer>
+      <Container style={{ minHeight: "80vh" }}>{children}</Container>
+      <footer
+        style={{
+          textAlign: "center",
+          borderTop: "#fff solid 1px",
+          paddingTop: "12px",
+        }}
+      >
+        All Right Reserved. @{datae} Amazonn.
+      </footer>
     </div>
   );
 };
---
+++ utils/date.js
@@ -0,0 +1,6 @@
+const date = () => {
+  let date = new Date();
+  return date.getFullYear();
+};
+
+export default date;
